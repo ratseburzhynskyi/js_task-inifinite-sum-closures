9d9a342f5841003f3c78d62d97bb2708af7e6751
Oleksandr Ratseburzhynskyi <ratseburzhynskyi@gmail.com>
2025-09-17 17:13:04 +0300

    fixed

--- src/makeInfinityAdder.js
+++ src/makeInfinityAdder.js
@@ -3,7 +3,7 @@
 /***
  * @return {function}
  */
-function makeInfinityAdder() {
+function makeAdder() {
   let amount = 0;
 
   const adder = (...numbers) => {
@@ -23,4 +23,4 @@ function makeInfinityAdder() {
   return adder;
 }
 
-module.exports = makeInfinityAdder;
+module.exports = makeAdder;
